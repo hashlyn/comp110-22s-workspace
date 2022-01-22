5b94d8039474c2745fdc21ee2e8d41c3ec93a533
Hannah Wood <hwood1@unc.edu>
2022-01-22 14:52:33 -0500

    Exercise 1 Changes

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,9 +5,11 @@ __author__ = "730412456"
 five_character_word: str = input("Enter a 5-character word: ")
 if len(five_character_word) != 5: 
     print("Error: Word must contain 5 characters")
+    exit()
 single_character: str = input("Enter a single character: ")
 if len(single_character) != 1:
     print("Error: Character must be a single character")
+    exit()
 instances: int = 0
 print("Searching for " + single_character + " in " + five_character_word)
 
@@ -27,7 +29,9 @@ if single_character[0] == five_character_word[4]:
     instances = instances + 1 
     print(single_character + " found at index 4")
 
-if instances >= 1: 
+if instances > 1: 
     print(str(instances) + " instances of " + single_character + " found in " + five_character_word)
+if instances == 1:
+    print(str(instances) + " instance of " + single_character + " found in " + five_character_word)
 else:
     print("No instances of " + single_character + " found in " + five_character_word)
