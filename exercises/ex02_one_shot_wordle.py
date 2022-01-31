e3760b651ac1beaaf65dd8236707514949036567
Hannah Wood <hwood1@unc.edu>
2022-01-30 19:22:44 -0500

    Progress on EX02

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,38 @@
+"""EX02 - One Shot Wordle - An even cuter step towards Wordle."""
+
+__author__ = "730412456"
+
+secret_word: str = "python"
+six_letter_guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
+
+while len(six_letter_guess) != len(secret_word):
+    six_letter_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
+
+index: int = 0
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+resulting_emoji: str = " "
+
+while index < len(secret_word): 
+    if six_letter_guess[index] == secret_word[index]:
+        resulting_emoji: str = resulting_emoji + GREEN_BOX
+    else:
+        letter_found_anywhere: bool = False
+        alternate_indices: int = 0
+        while letter_found_anywhere is not True and alternate_indices < len(secret_word):
+            if alternate_indices == six_letter_guess[index]: 
+                letter_found_anywhere: bool = True
+            else: 
+                alternate_indices: int = alternate_indices + 1
+        if letter_found_anywhere is True: 
+            resulting_emoji: str = resulting_emoji + YELLOW_BOX
+        else:
+            resulting_emoji: str = resulting_emoji + WHITE_BOX 
+    index: int = index + 1 
+print(resulting_emoji)
+
+if six_letter_guess == secret_word:
+    print("Woo! You got it.")
+else: 
+    print("Not quite. Try again soon.")
